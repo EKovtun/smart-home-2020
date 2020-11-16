e90ce5f78993bea26376c6e64b5147f739b7fc04
ekovtun95 <ekovtun95@yandex.ru>
2020-11-16 12:09:46 +0300

    Добавил final модификатор

--- src/main/java/ru/sbt/mipt/oop/events/adapter/MapBasedSensorEventFactory.java
+++ src/main/java/ru/sbt/mipt/oop/events/adapter/MapBasedSensorEventFactory.java
@@ -5,13 +5,11 @@ import ru.sbt.mipt.oop.events.SensorEventType;
 import java.util.Map;
 
 public class MapBasedSensorEventFactory implements SensorEventFactory {
-    private Map<String, SensorEventType> mapTypes;
+    private final Map<String, SensorEventType> mapTypes;
 
     public MapBasedSensorEventFactory(Map<String, SensorEventType> mapTypes) {
         this.mapTypes = mapTypes;
     }
-
-    public MapBasedSensorEventFactory() {}
 
     @Override
     public SensorEventType getType(String typeName) {
